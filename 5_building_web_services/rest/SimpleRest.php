39cd89c8b1f3053079828bd82df1406c9e392e11
Francisco Couto <franciscomcouto@gmail.com>
2019-04-05 10:09:50 +0100

    update

--- 5_building_web_services/rest/SimpleRest.php
+++ 5_building_web_services/rest/SimpleRest.php
@@ -58,7 +58,7 @@ class SimpleRest {
 			503 => 'Service Unavailable',  
 			504 => 'Gateway Timeout',  
 			505 => 'HTTP Version Not Supported');
-		return ($httpStatus[$statusCode] ? $httpStatus[$statusCode] : $httpStatus[500]);
+		return ($httpStatus[$statusCode]) ? $httpStatus[$statusCode] : $status[500];
 	}
 }
 ?>
